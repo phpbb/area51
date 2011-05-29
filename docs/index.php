fe826e3350c36f9b539db95e16ce8ae1a65065e2
Nils Adermann <naderman@naderman.de>
2011-05-29 09:49:37 +0200

    Current Area51 site with new logo & phpbb.com backlink

---
+++ docs/index.php
@@ -0,0 +1,3 @@
+<?php
+	header('Location: http://area51.phpbb.com/');
+?>
---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="description" content="area51 @ phpBB - The development site of phpBB Group" />
+	<meta name="keywords" content="Forum software, discussion software, free, opensource, development" />
+	<title>area51 @ phpBB.com</title>
+	<link href="area51.css" media="all" rel="stylesheet" type="text/css" />
+</head>
+<body>
+	<div id="wrapper">
+		<div id="phpbb-header-bar">
+		<ul class="phpbb-menu">
+			<li class="phpbb-logo"><a style="width:222px;" href="http://area51.phpbb.com/"><img src="/images/area51.png" alt="area51 @ phpBB" title="area51.phpbb.com" /></a></li>
+			<li class="active"><a href="http://area51.phpbb.com">Home</a></li>
+			<li><a href="http://tracker.phpbb.com/secure/Dashboard.jspa">Tracker</a></li>
+			<li><a href="http://wiki.phpbb.com/">Wiki</a></li>
+			<li><a href="http://bamboo.phpbb.com/">Builds</a></li>
+			<li><a href="http://code.phpbb.com/">Code Forge</a></li>
+			<li><a href="http://area51.phpbb.com/phpBB/">Discussion</a></li>
+		</ul>
+		<div style="float:right;margin-top:3px;">
+			<a href="http://www.phpbb.com/"><img src="/images/back_phpbb.png" alt="phpBB" title="www.phpbb.com" /></a>
+		</div>
+		</div>
+
+		<div id="header">
+			<h1>area51 @ phpBB.com</h1>
+		</div>
+
+		<div id="main">
+			<div id="content">
+				<h2>Welcome to area51, home to development of phpBB Group products.</h2>
+
+				<p>Remember, this is the home to <b>development</b> of phpBB. If you want the latest stable release of the software, need support, etc. please visit the main site at <a href="http://www.phpbb.com/">www.phpbb.com</a>. Please do not post support questions on the boards here!</p>
+
+				<p style="font-weight:bold;"><a href="/phpBB/">Visit the Development Board</a></p>
+		    
+				<div class="box">
+					<h3>phpBB Development Sites and Tools</h3>
+					<ul>
+						<li>
+							<a href="http://tracker.phpbb.com">Ticket Tracker</a>
+							<p>Report bugs, request features or improvements on the ticket tracker.</p>
+						</li>
+						<li>
+							<a href="http://wiki.phpbb.com">Wiki</a>
+							<p>A wiki containing all the information related to developing MODs for phpBB and working on phpBB itself.</p>
+						</li>
+						<li>
+							<a href="http://code.phpbb.com">Code Forge</a>
+							<p>You can find information on the repositories as well as mailinglists of various phpBB projects on this site.</p>
+						</li>
+						<li>
+							<a href="http://area51.phpbb.com/phpBB/">area51 Development Board</a>
+							<p>All discussion of the development of phpBB takes place on this board. It's running on the latest code from the development repository.</p>
+						</li>
+						<li>
+							<a href="http://github.com/phpbb">phpBB's github page</a>
+							<p>The phpBB page on github, where the source code repositories are hosted.</p>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+	</div>
+</body>
+</html>
