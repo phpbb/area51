ee06061db5fdef8d993f6904f2b7d3d3a464955d
Nils Adermann <naderman@naderman.de>
2011-06-01 05:30:11 +0200

    Redesign the area51 home page

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 		<ul class="phpbb-menu">
 			<li class="phpbb-logo"><a href="http://area51.phpbb.com/"><img src="/images/area51.png" alt="area51 @ phpBB" title="area51.phpbb.com" /></a></li>
 			<li class="active"><a href="http://area51.phpbb.com">Home</a></li>
-			<li><a href="http://tracker.phpbb.com/secure/Dashboard.jspa">Tracker</a></li>
+			<li><a href="http://tracker.phpbb.com/">Tracker</a></li>
 			<li><a href="http://wiki.phpbb.com/">Wiki</a></li>
 			<li><a href="http://bamboo.phpbb.com/">Builds</a></li>
 			<li><a href="http://code.phpbb.com/">Code Forge</a></li>
@@ -32,7 +32,87 @@
 		<div id="main">
 			<div id="content">
 
-				<p>Remember, this is the home to <b>development</b> of phpBB. If you want the latest stable release of the software, need support, etc. please visit the main site at <a href="http://www.phpbb.com/">www.phpbb.com</a>. Please do not post support questions on the boards here!</p>
+				<h2>Get Involved</h2>
+
+
+				<div>
+					<h2>Development Sites &amp; Tools</h2>
+					<ul class="section-list">
+						<li>
+							<a href="http://github.com/phpbb/" title="Github"><img src="" title="Github" alt="Github" /></a>
+							<h2>Github</h2>
+							<p>phpBB's source code is hosted in <a href="http://git-scm.org/" title="Git">Git</a> repositories at Github. You can also find a number of other tools for phpBB or MOD development there.</p>
+						</li>
+						<li>
+							<a href="http://tracker.phpbb.com/" title="Tracker"><img src="" title="Tracker" alt="Tracker" /></a>
+							<h2>Issue Tracker</h2>
+							<p>If you found a bug in phpBB this is the place to report it. If you're not sure if the behaviour you found is really a bug try asking for help in the <a href="http://www.phpbb.com/community/viewforum.php?f=46" title="Support Forum">Support Forum</a> first.</p>
+						</li>
+						<li>
+							<a href="http://bamboo.phpbb.com/" title="Bamboo"><img src="" title="Bamboo" alt="Bamboo" /></a>
+							<h2>Continuous Integration</h2>
+							<p>phpBB is continuously tested against a large suite of automated tests to ensure we deliver a high quality product. Bamboo takes care of running the tests and providing us with useful statistics.</p>
+						</li>
+						<li>
+							<a href="http://wiki.phpbb.com/" title="Wiki"><img src="" title="Wiki" alt="Wiki" /></a>
+							<h2>Wiki</h2>
+							<p>The wiki is used to document a variety of best practices as well as the architecture of phpBB. A number of tutorials and in-depth articles explain how to work with particular parts of phpBB.</p>
+						</li>
+
+						<li class="line-break">
+							<a href="http://code.phpbb.com" title="Code Forge"><img src="" title="Code Forge" alt="Code Forge" /></a>
+							<h2>Code Forge</h2>
+							<p>The former home of phpBB still hosting subversion repository mirrors of our git repositories as well as a few utilities that haven't been moved over to git yet.</p>
+						</li>
+						<li>
+							<img src="" title="Coding Guidelines" alt="Coding Guidelines" />
+							<h2>Coding Guidelines</h2>
+							<p>Automatically generated documentation for phpBB APIs - always up to date.</p>
+							<ul>
+								<li><a href="docs/30x/coding-guidelines.html">Olympus (3.0.x) Guide</a></li>
+								<li><a href="docs/31x/coding-guidelines.html">Ascraeus (3.1.x) Guide</a></li>
+							</ul>
+						</li>
+						<li>
+							<img src="" title="API Documentation" alt="API Documentation" />
+							<h2>API Documentation</h2>
+							<p>Automatically generated documentation for phpBB APIs - always up to date.</p>
+							<ul>
+								<li><a href="docs/code/30x/">Olympus (3.0.x) Docs</a></li>
+								<li><a href="docs/code/31x/">Ascraeus (3.1.x) Docs</a></li>
+							</ul>
+						</li>
+						<li>
+							<a href="phpBB/" title="Development Discussion"><img src="" title="Development Discussion" alt="Development Discussion" /></a>
+							<h2>Development Discussion</h2>
+							<p>All proposals for new features and code changes are discussed in this forum. It runs the latest unstable version of phpBB allowing you to test upcoming features.</p>
+						</li>
+					</ul>
+				</div>
+
+<!--
+				bamboo
+				wiki
+				security tracker
+				get involved
+					-> qa
+					-> code
+					-> bugs
+					-> ...
+					-> wiki
+				coding guidelines
+				code forge
+				old bug tracker
+				qa forum?
+				discussion board / rfcs
+-->
+
+
+
+
+
+
+				<p style="margin-top:1000px;">Remember, this is the home to <b>development</b> of phpBB. If you want the latest stable release of the software, need support, etc. please visit the main site at <a href="http://www.phpbb.com/">www.phpbb.com</a>. Please do not post support questions on the boards here!</p>
 
 				<p style="font-weight:bold;"><a href="/phpBB/">Visit the Development Discussion Board</a></p>
 
