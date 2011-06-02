71f116ea1b657f53c602f9f3d6bacf0f328b3854
dellsystem <ilostwaldo@gmail.com>
2011-06-02 00:47:37 -0400

    add remaining images

--- index.php
+++ index.php
@@ -74,28 +74,28 @@
 							<p>phpBB's source code is hosted in <a href="http://git-scm.org/" title="Git">Git</a> repositories at Github. You can also find a number of other tools for phpBB or MOD development there.</p>
 						</li>
 						<li>
-							<a href="http://tracker.phpbb.com/" title="Tracker"><img src="" title="Tracker" alt="Tracker" /></a>
+							<a href="http://tracker.phpbb.com/" title="Tracker"><img src="images/tracker.png" title="Tracker" alt="Tracker" /></a>
 							<h2>Issue Tracker</h2>
 							<p>If you found a bug in phpBB this is the place to report it. If you're not sure if the behaviour you found is really a bug try asking for help in the <a href="http://www.phpbb.com/community/viewforum.php?f=46" title="Support Forum">Support Forum</a> first.</p>
 						</li>
 						<li>
-							<a href="http://bamboo.phpbb.com/" title="Bamboo"><img src="" title="Bamboo" alt="Bamboo" /></a>
+							<a href="http://bamboo.phpbb.com/" title="Bamboo"><img src="images/bamboo.png" title="Bamboo" alt="Bamboo" /></a>
 							<h2>Continuous Integration</h2>
 							<p>phpBB is continuously tested against a large suite of automated tests to ensure we deliver a high quality product. Bamboo takes care of running the tests and providing us with useful statistics.</p>
 						</li>
 						<li>
-							<a href="http://wiki.phpbb.com/" title="Wiki"><img src="" title="Wiki" alt="Wiki" /></a>
+							<a href="http://wiki.phpbb.com/" title="Wiki"><img src="images/wiki.png" title="Wiki" alt="Wiki" /></a>
 							<h2>Wiki</h2>
 							<p>The wiki is used to document a variety of best practices as well as the architecture of phpBB. A number of tutorials and in-depth articles explain how to work with particular parts of phpBB.</p>
 						</li>
 
 						<li class="line-break">
-							<a href="http://code.phpbb.com" title="Code Forge"><img src="" title="Code Forge" alt="Code Forge" /></a>
+							<a href="http://code.phpbb.com" title="Code Forge"><img src="images/code-forge.png" title="Code Forge" alt="Code Forge" /></a>
 							<h2>Code Forge</h2>
 							<p>The former home of phpBB source code still hosts a few utilities that haven't been moved over to git yet and subversion mirrors of our git repositories.</p>
 						</li>
 						<li>
-							<img src="" title="Coding Guidelines" alt="Coding Guidelines" />
+							<img src="images/coding-style.png" title="Coding Guidelines" alt="Coding Guidelines" />
 							<h2>Coding Guidelines</h2>
 							<p>Automatically generated documentation for phpBB APIs - always up to date.</p>
 							<ul>
@@ -104,7 +104,7 @@
 							</ul>
 						</li>
 						<li>
-							<img src="" title="API Documentation" alt="API Documentation" />
+							<img src="images/api.png" title="API Documentation" alt="API Documentation" />
 							<h2>API Documentation</h2>
 							<p>Automatically generated documentation for phpBB APIs - always up to date.</p>
 							<ul>
@@ -113,7 +113,7 @@
 							</ul>
 						</li>
 						<li>
-							<a href="phpBB/" title="Development Discussion"><img src="" title="Development Discussion" alt="Development Discussion" /></a>
+							<a href="phpBB/" title="Development Discussion"><img src="images/development-discussion.png" title="Development Discussion" alt="Development Discussion" /></a>
 							<h2>Development Discussion</h2>
 							<p>All proposals for new features and code changes are discussed in this forum. It runs the latest unstable version of phpBB allowing you to test upcoming features.</p>
 						</li>
