4068a9e09c05cf465e277a6ca7274f1ccf6dc5ef
Unknown Bliss <m@michaelcullum.com>
2012-11-22 22:50:34 +0000

    Revert "Add tempoary test script"
    This reverts commit 75854b904dd83f5a07e36ede2bd5981809bbcee1.

--- web/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$w = stream_get_wrappers();
-echo 'openssl: ',  extension_loaded  ('openssl') ? 'yes':'no', "\n";
-echo 'http wrapper: ', in_array('http', $w) ? 'yes':'no', "\n";
-echo 'https wrapper: ', in_array('https', $w) ? 'yes':'no', "\n";
-echo 'wrappers: ', var_dump($w);
