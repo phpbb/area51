3aa8cb8ab919d23bc939107a1dba11ab5aaa2132
Andreas Fischer <bantu@phpbb.com>
2014-05-08 13:39:21 +0200

    update_phpbb.sh: Add variables for remote and branch.

--- bin/update_phpbb.sh
+++ bin/update_phpbb.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+REMOTE=origin
+BRANCH=master
+
 cd area51-phpbb3
-git fetch origin
-git reset --hard origin/master
+git fetch "$REMOTE"
+git reset --hard "$REMOTE/$BRANCH"
 php -f ./phpBB/bin/phpbbcli.php db:migrate
 
