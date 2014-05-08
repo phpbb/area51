d45d9e2c87df5349fb0483b04b3e106d7a46aa81
Andreas Fischer <bantu@phpbb.com>
2014-05-08 13:49:35 +0200

    update_phpbb.sh: Also update composer dependencies.

--- bin/update_phpbb.sh
+++ bin/update_phpbb.sh
@@ -5,12 +5,13 @@ REMOTE=origin
 BRANCH=master
 
 HERE=$(dirname "$0")
-cd "$HERE/../area51-phpbb3"
+cd "$HERE/../area51-phpbb3/phpBB"
 git fetch "$REMOTE"
 
 if [ `git rev-parse "$BRANCH"` != `git rev-parse "$REMOTE/$BRANCH"` ]
 then
 	git reset --hard "$REMOTE/$BRANCH"
-	phpBB/bin/phpbbcli.php db:migrate
-	phpBB/bin/phpbbcli.php cache:purge
+	../composer.phar install --no-dev
+	bin/phpbbcli.php db:migrate
+	bin/phpbbcli.php cache:purge
 fi
