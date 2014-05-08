4b01e82f1c0a684bcc6cb8be56007d6092c593af
Andreas Fischer <bantu@phpbb.com>
2014-05-08 13:42:39 +0200

    update_phpbb.sh: Only migrate when branches are different.

--- bin/update_phpbb.sh
+++ bin/update_phpbb.sh
@@ -6,6 +6,9 @@ BRANCH=master
 
 cd area51-phpbb3
 git fetch "$REMOTE"
-git reset --hard "$REMOTE/$BRANCH"
-php -f ./phpBB/bin/phpbbcli.php db:migrate
 
+if [ `git rev-parse "$BRANCH"` != `git rev-parse "$REMOTE/$BRANCH"` ]
+then
+	git reset --hard "$REMOTE/$BRANCH"
+	php -f ./phpBB/bin/phpbbcli.php db:migrate
+fi
