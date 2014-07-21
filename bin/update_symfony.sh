5ba268fbbc26666248271d3c946114ef0d125ab9
Andreas Fischer <bantu@phpbb.com>
2014-07-21 11:57:34 +0200

    Add script for updating symfony.

---
+++ bin/update_symfony.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+
+REMOTE=origin
+BRANCH=master
+
+HERE=$(dirname "$0")
+cd "$HERE/../"
+git fetch "$REMOTE"
+
+if [ `git rev-parse "$BRANCH"` != `git rev-parse "$REMOTE/$BRANCH"` ]
+then
+	git reset --hard "$REMOTE/$BRANCH"
+	../composer.phar install --no-dev
+	app/console cache:clear --env=prod
+	app/console cache:warmup --env=prod
+fi
