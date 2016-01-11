d1552ef073b1b004b7cba0e3d2a518d839ce17a5
MichaelC <m@michaelcullum.com>
2016-01-11 00:39:59 +0000

    phpBB 3.1.7

--- bin/build_code_changes_31.sh
+++ bin/build_code_changes_31.sh
@@ -3,8 +3,8 @@ mode="side-by-side"
 basedir=`cd $(dirname "$BASH_SOURCE"); cd ..; pwd`
 outdir=`cd "$basedir"; cd ./web/code-changes/; pwd`
 datadir=`cd "$basedir"; cd ./data/code-changes/; pwd`
-latest="3.1.6"
-releases="3.1.0 3.1.1 3.1.2 3.1.3 3.1.4 3.1.5"
+latest="3.1.7-PL1"
+releases="3.1.0 3.1.1 3.1.2 3.1.3 3.1.4 3.1.5 3.1.6 3.1.7"
 
 # Set up the file structure
 if [ ! -d "$datadir/repo" ];
