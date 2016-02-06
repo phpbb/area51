0c0a319d508f56fd22fe914e0eb60b897b7f62d5
MichaelC <m@michaelcullum.com>
2016-02-06 22:00:29 +0000

    Actually this is all just a bit broken legacy. Lets nuke it

--- src/Phpbb/Area51Bundle/Controller/DefaultController.php
+++ src/Phpbb/Area51Bundle/Controller/DefaultController.php
@@ -84,18 +84,6 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/docs{path}", requirements={"path"=".*"}, defaults={"path"=""})
-     */
-    public function docsRedirectAction($path)
-    {
-        if (preg_match('#^/3#', $path)) {
-            return $this->redirect('http://area51.phpbb.com/docs/30x', 301);
-        }
-
-        return $this->redirect($this->generateUrl('index'), 301);
-    }
-
-    /**
      * @Route("/downloads/", name="downloads")
      * @Template()
      */
