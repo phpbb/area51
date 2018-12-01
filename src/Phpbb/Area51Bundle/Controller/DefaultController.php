fc1fa093534b3bf4c147c11e8eb9c5cbbe791e77
Marc Alexander <admin@m-a-styles.de>
2018-12-01 17:56:00 +0100

    Update for 3.2.5-RC1 release

--- src/Phpbb/Area51Bundle/Controller/DefaultController.php
+++ src/Phpbb/Area51Bundle/Controller/DefaultController.php
@@ -101,11 +101,11 @@ class DefaultController extends Controller
     public function downloadsAction()
     {
         // Make this false when the most recent release is not an RC/Alpha/Beta
-        $latestDevelopment = false;
+        $latestDevelopment = true;
 
-        $previousVersions = ['3.2.3', '3.2.2', '3.2.1', '3.2.0', '3.1.12', '3.1.11', '3.1.10', '3.1.9', '3.1.8', '3.1.7-pl1', '3.1.6', '3.1.5', '3.1.4', '3.1.3', '3.1.2', '3.1.1', '3.1.0'];
-        $currentVersion = '3.2.4-RC1';
-        $mainPackageSha = 'eff6874519980a9c123841010e7ee042cc4f8fe7cf086d0674c5268e000c6fa8';
+        $previousVersions = ['3.2.4', '3.2.3', '3.2.2', '3.2.1', '3.2.0', '3.1.12', '3.1.11', '3.1.10', '3.1.9', '3.1.8', '3.1.7-pl1', '3.1.6', '3.1.5', '3.1.4', '3.1.3', '3.1.2', '3.1.1', '3.1.0'];
+        $currentVersion = '3.2.5-RC1';
+        $mainPackageSha = '8d70bd6d9a5f6aca5fe973e76a1a142d9d92f7e5d694d84aa8fce6c7d697d1ae';
         $currentBranch = '3.2/unstable';
         $currentVersionFiles =  'https://download.phpbb.com/pub/release/' . $currentBranch
             . '/' . $currentVersion . '/';
