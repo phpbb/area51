a5d787fae6211f98c142a18a4130ba0b678dfcba
Marc Alexander <admin@m-a-styles.de>
2019-04-29 08:57:45 +0200

    Build code changes for 3.2.6

--- bin/build_code_changes_32.sh
+++ bin/build_code_changes_32.sh
@@ -3,8 +3,8 @@ mode="side-by-side"
 basedir=`cd $(dirname "$BASH_SOURCE"); cd ..; pwd`
 outdir=`cd "$basedir"; cd ./web/code-changes/; pwd`
 datadir=`cd "$basedir"; cd ./data/code-changes/; pwd`
-latest="3.2.5"
-releases="3.2.0 3.2.1 3.2.2 3.2.3 3.2.4"
+latest="3.2.6"
+releases="3.2.0 3.2.1 3.2.2 3.2.3 3.2.4 3.2.5"
 
 # Set up the file structure
 if [ ! -d "$datadir/repo" ];
