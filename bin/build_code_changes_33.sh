4f8bc6bbf76b02db9ab45f84a06bb94f8103e5be
Marc Alexander <admin@m-a-styles.de>
2019-12-26 17:35:12 +0100

    Update 3.3.x code changes

--- bin/build_code_changes_33.sh
+++ bin/build_code_changes_33.sh
@@ -3,8 +3,8 @@ mode="side-by-side"
 basedir=`cd $(dirname "$BASH_SOURCE"); cd ..; pwd`
 outdir=`cd "$basedir"; cd ./web/code-changes/; pwd`
 datadir=`cd "$basedir"; cd ./data/code-changes/; pwd`
-latest="3.3.0-b1"
-releases="3.3.0-b1"
+latest="3.3.0-RC1"
+releases="3.3.0-b1 3.3.0-b2"
 
 # Set up the file structure
 if [ ! -d "$datadir/repo" ];
