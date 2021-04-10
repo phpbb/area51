53aa1049aedb855848cf2fbe1ea400802ee6a176
Marc Alexander <admin@m-a-styles.de>
2021-04-10 11:22:43 +0200

    Remove workaround to prevent update to specific commit

--- bin/update_phpbb.sh
+++ bin/update_phpbb.sh
@@ -9,7 +9,7 @@ HERE=$(dirname "$0")
 cd "$HERE/../area51-phpbb3/phpBB"
 git fetch "$REMOTE"
 
-if [ `git rev-parse "$BRANCH"` != `git rev-parse "$REMOTE/$BRANCH"` ] && [ `git rev-parse "$BRANCH"` != beee215bc2d28159b37d5e24e37598fd3d03edbd ]
+if [ `git rev-parse "$BRANCH"` != `git rev-parse "$REMOTE/$BRANCH"` ]
 then
 	git reset --hard "$REMOTE/$BRANCH"
 	../composer.phar install --no-dev --optimize-autoloader
