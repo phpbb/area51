03a4c3d431a65a345647b1d3246953229a4a7116
Marc Alexander <admin@m-a-styles.de>
2022-03-15 21:06:02 +0100

    Add 3.3.6 to code changes

--- bin/build_code_changes_33.sh
+++ bin/build_code_changes_33.sh
@@ -3,8 +3,8 @@ mode="side-by-side"
 basedir=`cd $(dirname "$BASH_SOURCE"); cd ..; pwd`
 outdir=`cd "$basedir"; cd ./web/code-changes/; pwd`
 datadir=`cd "$basedir"; cd ./data/code-changes/; pwd`
-latest="3.3.5"
-releases="3.3.0-b1 3.3.0-b2 3.3.0-RC1 3.3.0 3.3.1 3.3.2 3.3.3 3.3.4"
+latest="3.3.6"
+releases="3.3.0-b1 3.3.0-b2 3.3.0-RC1 3.3.0 3.3.1 3.3.2 3.3.3 3.3.4 3.3.5"
 
 # Set up the file structure
 if [ ! -d "$datadir/repo" ];
