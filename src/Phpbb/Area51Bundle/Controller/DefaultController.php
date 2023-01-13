5dc85c8f82730fb968a6b803813c040ce18cb40a
Marc Alexander <admin@m-a-styles.de>
2023-01-13 22:32:52 +0100

    Update file hash for 3.3.10-RC1 package

--- src/Phpbb/Area51Bundle/Controller/DefaultController.php
+++ src/Phpbb/Area51Bundle/Controller/DefaultController.php
@@ -126,7 +126,7 @@ class DefaultController extends Controller
 
         $previousVersions = ['3.3.9', '3.3.8', '3.3.7', '3.3.6', '3.3.5', '3.3.4', '3.3.3', '3.3.2', '3.3.1', '3.3.0', '3.2.10', '3.2.9', '3.2.8', '3.2.7', '3.2.6', '3.2.5', '3.2.4', '3.2.3', '3.2.2', '3.2.1', '3.2.0', '3.1.12', '3.1.11', '3.1.10', '3.1.9', '3.1.8', '3.1.7-pl1', '3.1.6', '3.1.5', '3.1.4', '3.1.3', '3.1.2', '3.1.1', '3.1.0'];
         $currentVersion = '3.3.10-RC1';
-        $mainPackageSha = 'eb79e2c46d21641f4af1ee6bcbf0832c03add17de78da007bbd4127c199afc8d';
+        $mainPackageSha = 'a088e7b31f48c31e0eb17a7788bf47a7492ca2ed2e639a8934032f6e37942561';
         $currentBranch = '3.3/unstable';
         $currentVersionFiles =  'https://download.phpbb.com/pub/release/' . $currentBranch
             . '/' . $currentVersion . '/';
