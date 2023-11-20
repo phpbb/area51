b149c361dc29ea04100cc0e7f25e7dca301c5d96
Marc Alexander <admin@m-a-styles.de>
2023-11-20 20:38:42 +0100

    Change doctrine-bundle to ^2.1

--- config/bundles.php
+++ config/bundles.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
