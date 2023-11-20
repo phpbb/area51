9b03cf4ebe603e6d978be4d9c67ddc5ee7b5804f
Marc Alexander <admin@m-a-styles.de>
2023-11-20 21:29:12 +0100

    Update to Symfony 6.3

--- src/TrackerChartFactory.php
+++ src/TrackerChartFactory.php
@@ -2,11 +2,15 @@
 
 namespace App;
 
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+
 class TrackerChartFactory
 {
     protected $root_path;
 
-    public function __construct($root_path)
+    public function __construct(
+        #[Autowire(env: 'tracker_chart_root_path')]
+        string $root_path)
     {
         $this->root_path = $root_path;
     }
